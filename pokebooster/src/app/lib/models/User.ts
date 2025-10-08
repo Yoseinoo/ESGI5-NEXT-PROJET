2f236e8d9f7a29702f6efd2670df849cd27cbd72
Yoseinoo <92569285+Yoseinoo@users.noreply.github.com>
2025-10-08 17:09:36 +0200

    Added mongo connection

---
+++ pokebooster/src/app/lib/models/User.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+export interface IUser extends Document {
+  name: string;
+  email: string;
+}
+
+const UserSchema: Schema<IUser> = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+});
+
+// Avoid model overwrite issues in dev (Next.js reloads)
+export const User: Model<IUser> = mongoose.models.User || mongoose.model<IUser>("User", UserSchema);
--- pokebooster/src/app/page.tsx
+++ pokebooster/src/app/page.tsx
@@ -1,10 +1,17 @@
 import SetsContainer from "./components/SetsContainer";
 import { getSets } from "./lib/externalApi";
+import { User } from "./lib/models/User";
+import { connectToDatabase } from "./lib/mongodb";
 
 export default async function Home() {
     console.log('EXTERNAL_API_URL:', process.env.EXTERNAL_API_URL);
 
     const sets = await getSets();
+
+    //Test db connection
+    await connectToDatabase();
+    const users = await User.find();
+    console.log(users);
 
     return (
         <div>
